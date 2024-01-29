1bdb662280b824ecd0d6b9026789c8344cc21ced
박지용 <gdragon0228@naver.com>
2024-01-29 17:56:19 +0900

    study day4

---
+++ lib/screen/homr_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          '버튼',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.green;
+                    }
+                    return Colors.black;
+                  },
+                ),
+                foregroundColor: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.pressed)) {
+                      return Colors.white;
+                    }
+                    return Colors.red;
+                  },
+                ),
+                padding: MaterialStateProperty.resolveWith(
+                  (Set<MaterialState> states) {
+                    return EdgeInsets.all(20);
+                  },
+                ),
+              ),
+              child: Text(
+                'ButtonStyle',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                //메인컬러
+                onPrimary: Colors.black,
+                //글자, 애니메이션 색깔
+                shadowColor: Colors.green,
+                //그림자 색깔
+                elevation: 10,
+                //튀어나오는 정도
+                textStyle:
+                    TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
+                padding: EdgeInsets.all(30),
+                side: BorderSide(color: Colors.black, width: 4.0),
+              ),
+              child: Text('ElevatedButton'),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                primary: Colors.green,
+                backgroundColor: Colors.yellow,
+                elevation: 10.0,
+              ),
+              child: Text('OutlinedButton'),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                primary: Colors.brown,
+                backgroundColor: Colors.blue,
+              ),
+              child: Text('TextButton'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
